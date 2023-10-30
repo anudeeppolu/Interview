acd55afed1e344431ec4090e0b165e34d53bd538
anudeeppolu <113202068+anudeeppolu@users.noreply.github.com>
2023-10-30 16:47:25 +0530

    Add files via upload

---
+++ assignment/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import UserDisplay from './components/UserDisplay';
+
+function App() {
+  return (
+    <div className="App">
+  
+    <UserDisplay/>
+    </div>
+  );
+}
+
+export default App;
